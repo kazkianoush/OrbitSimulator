6a0da30b17a8894f2b7aad66ecc21ef981a2280e
kaz03 <kazkianoush03@gmail.com>
2022-10-11 14:26:11 -0700

    update 1

---
+++ src/main/ui/SetupInterface.java
@@ -0,0 +1,21 @@
+package ui;
+
+import model.Planet;
+import model.Shuttle;
+
+import java.util.ArrayList;
+
+interface SetupInterface {
+    ArrayList<Object> getSetupPlanet();
+
+    Planet initPlanet();
+
+    ArrayList<Object> getSetupShuttle();
+
+    Shuttle initShuttle();
+
+    void initGame(Planet p, Shuttle s);
+
+
+
+}
